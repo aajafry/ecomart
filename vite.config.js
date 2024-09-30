976cc863b4acdf6664874b9634968ab8f05a42a0
aajafry <aajafry2@gmail.com>
2024-09-30 15:57:46 +0600

    ecomart: avoid log warning

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import react from '@vitejs/plugin-react';
-import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-     visualizer({ open: true }) 
-  ],
+  plugins: [react()],
   build: {
     chunkSizeWarningLimit: 1200, // extended chunk size
   },
